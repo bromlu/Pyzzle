8ba74ab5fa0c3745b6f9b9716630e58eda970fd2
bromlu <lukewilliambrom@gmail.com>
2019-04-08 14:58:14 -0400

    Restucture example to use objects

--- demos/basic/basicPuzzleGame.py
+++ demos/basic/basicPuzzleGame.py
@@ -6,40 +6,46 @@ from pyzzle import input
 WIDTH = 800
 HEIGHT = 800
 
-gameObjects = { }
+mainCharacter = None
+
+class MainCharacter:
+    def __init__(self):
+        self.index = game.createGameObject()
+        game.setGameObjectPosition(self.index, WIDTH/2, HEIGHT/2)
+        sprites.addSprite(self.index, "MainCharacter.png")
+        sprites.setStartFrame(self.index, 0, 64, 24, 32)
+
+        self.runUpAnimation = animations.addAnimation(self.index, 0)
+        self.runDownAnimation = animations.addAnimation(self.index, 32)
+        self.runRightAnimation = animations.addAnimation(self.index, 64)
+        self.runLeftAnimation = animations.addAnimation(self.index, 96)
+
 
 if __name__ == "__main__":
     game.init("basicPuzzleGame", "Basic Puzzle Game", WIDTH, HEIGHT)
 
 def init():
-    global gameObjects
-    gameObjects["MainCharacter"] = {}
-    gameObjects["MainCharacter"]["index"] = game.createGameObject()
-    sprites.addSprite(gameObjects["MainCharacter"]["index"], "MainCharacter.png")
-    sprites.setStartFrame(gameObjects["MainCharacter"]["index"], 0, 64, 24, 32)
-    game.setGameObjectPosition(gameObjects["MainCharacter"]["index"], WIDTH/2, HEIGHT/2)
-    gameObjects["MainCharacter"]["RunUpAnimation"] = animations.addAnimation(gameObjects["MainCharacter"]["index"], 0)
-    gameObjects["MainCharacter"]["RunRightAnimation"] = animations.addAnimation(gameObjects["MainCharacter"]["index"], 32)
-    gameObjects["MainCharacter"]["RunDownAnimation"] = animations.addAnimation(gameObjects["MainCharacter"]["index"], 64)
-    gameObjects["MainCharacter"]["RunLeftAnimation"] = animations.addAnimation(gameObjects["MainCharacter"]["index"], 96)
+    global mainCharacter
+    mainCharacter = MainCharacter()
+    print(mainCharacter.index)
 
 def update(): 
-    global gameObjects
+    global mainCharacter
     if input.isKeyPressed(22): #W
-        game.moveGameObject(gameObjects["MainCharacter"]["index"], 0, -10)
-        animations.play(gameObjects["MainCharacter"]["index"], gameObjects["MainCharacter"]["RunUpAnimation"])
+        game.moveGameObject(mainCharacter.index, 0, -10)
+        animations.play(mainCharacter.index, mainCharacter.runUpAnimation)
     if input.isKeyPressed(0): #A
-        game.moveGameObject(gameObjects["MainCharacter"]["index"], -10, 0)
-        animations.play(gameObjects["MainCharacter"]["index"], gameObjects["MainCharacter"]["RunLeftAnimation"])
+        game.moveGameObject(mainCharacter.index, -10, 0)
+        animations.play(mainCharacter.index, mainCharacter.runLeftAnimation)
     if input.isKeyPressed(3): #D
-        game.moveGameObject(gameObjects["MainCharacter"]["index"], 10, 0)
-        animations.play(gameObjects["MainCharacter"]["index"], gameObjects["MainCharacter"]["RunRightAnimation"])
+        game.moveGameObject(mainCharacter.index, 10, 0)
+        animations.play(mainCharacter.index, mainCharacter.runRightAnimation)
     if input.isKeyPressed(18): #S
-        game.moveGameObject(gameObjects["MainCharacter"]["index"], 0, 10)
-        animations.play(gameObjects["MainCharacter"]["index"], gameObjects["MainCharacter"]["RunDownAnimation"])
+        game.moveGameObject(mainCharacter.index, 0, 10)
+        animations.play(mainCharacter.index, mainCharacter.runDownAnimation)
     if not input.isKeyPressed(22) and not input.isKeyPressed(0) and not input.isKeyPressed(3) and not input.isKeyPressed(18) :
-        animations.stop(gameObjects["MainCharacter"]["index"], gameObjects["MainCharacter"]["RunUpAnimation"])
+        animations.stop(mainCharacter.index, mainCharacter.runUpAnimation)
 
 def draw(): 
-    global gameObjects
-    sprites.draw(gameObjects["MainCharacter"]["index"])
+    global mainCharacter
+    sprites.draw(mainCharacter.index)
