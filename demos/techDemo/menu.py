fd797cb543f7cfa305255eb000500ae51b2ba8a0
bromlu <lukewilliambrom@gmail.com>
2019-05-04 14:16:54 -0400

    Add more menu options

--- demos/techDemo/menu.py
+++ demos/techDemo/menu.py
@@ -11,12 +11,13 @@ currentlySelected = 0
 start = time.time()
 
 class MenuItem:
-    def __init__(self, text, x, y, w):
+    def __init__(self, text, x, y, w, action):
         self.text = text
         self.x = x
         self.y = y
         self.w = w
         self.selected = False
+        self.action = action
 
     def draw(self):
         if self.selected:
@@ -24,16 +25,48 @@ class MenuItem:
         else:
             text.draw(self.text, self.x, self.y, self.w)
 
+def gotoHome():
+    game.switchState("menu", "init", "update", "draw")
+
+def gotoPlay():
+    game.switchState("menu", "initPlay", "update", "draw")
+
+def gotoAbout():
+    game.switchState("menu", "initText", "update", "drawAbout")
+
+def gotoThanks():
+    game.switchState("menu", "initText", "update", "drawThanks")
+
 if __name__ == "__main__":
+    text.loadFont("trench100free.otf")
     game.init("menu", "Pyzzle Tech Demo", WIDTH, HEIGHT)
 
 def init():
     global menuItems
     global currentlySelected
-    text.loadFont("trench100free.otf")
-    menuItems.append(MenuItem("Play", WIDTH/2, HEIGHT/2 - 100, 72.0))
-    menuItems.append(MenuItem("About", WIDTH/2, HEIGHT/2, 72.0))
-    menuItems.append(MenuItem("Thanks", WIDTH/2, HEIGHT/2 + 100, 72.0))
+    menuItems = []
+    currentlySelected = 0
+    menuItems.append(MenuItem("Play", WIDTH/2, HEIGHT/2 - 100, 72.0, gotoPlay))
+    menuItems.append(MenuItem("About", WIDTH/2, HEIGHT/2, 72.0, gotoAbout))
+    menuItems.append(MenuItem("Thanks", WIDTH/2, HEIGHT/2 + 100, 72.0, gotoThanks))
+    menuItems[currentlySelected].selected = True
+
+def initPlay():
+    global menuItems
+    global currentlySelected
+    menuItems = []
+    currentlySelected = 0
+    menuItems.append(MenuItem("Pong", WIDTH/2, HEIGHT/2 - 100, 72.0, gotoHome))
+    menuItems.append(MenuItem("Space Explorer", WIDTH/2, HEIGHT/2, 72.0, gotoHome))
+    menuItems.append(MenuItem("Back", WIDTH/2, HEIGHT/2 + 100, 72.0, gotoHome))
+    menuItems[currentlySelected].selected = True
+
+def initText():
+    global menuItems
+    global currentlySelected
+    menuItems = []
+    currentlySelected = 0
+    menuItems.append(MenuItem("Back", WIDTH/2, HEIGHT - 100, 72.0, gotoHome))
     menuItems[currentlySelected].selected = True
 
 def update():
@@ -45,20 +78,33 @@ def update():
         return
     start = time.time()
 
-    if input.isKeyPressed(18) or input.isKeyPressed(74): #S
+    if input.isKeyPressed(18) or input.isKeyPressed(74): #S and Down
         menuItems[currentlySelected].selected = False
         currentlySelected = currentlySelected + 1
         if currentlySelected >= len(menuItems):
             currentlySelected = 0
         menuItems[currentlySelected].selected = True
-    if input.isKeyPressed(22) or input.isKeyPressed(73): #W
+    if input.isKeyPressed(22) or input.isKeyPressed(73): #W and Up
         menuItems[currentlySelected].selected = False
         currentlySelected = currentlySelected - 1
         if currentlySelected < 0:
             currentlySelected = len(menuItems) - 1
         menuItems[currentlySelected].selected = True
+    if input.isKeyPressed(58): # Enter
+        menuItems[currentlySelected].action()
 
-def draw():
-    text.draw("Welcome to the Pyzzle tech demo!", WIDTH/2, 100.0, 72.0)
+def drawMenu():
+    text.draw("Pyzzle Tech Demo", WIDTH/2, 100.0, 148.0)
     for item in menuItems:
         item.draw()
+
+def draw():
+    drawMenu()
+
+def drawAbout():
+    text.draw("This tech demo is meant to demostrate\n the capabilities of the Pyzzle game engine.", WIDTH + 30, HEIGHT/2, 72.0)
+    drawMenu()
+
+def drawThanks():
+    text.draw("Special Thanks to Imani, Caitlin, and Tim \n      for being great teammates.", WIDTH, HEIGHT/2, 72.0)
+    drawMenu()
