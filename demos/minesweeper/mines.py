d01e0a902869f80a29c715a7a932a3c07a6fd91d
tours <tim_ours@taylor.edu>
2019-05-06 15:49:31 -0400

    Added start to minesweeper.

---
+++ demos/minesweeper/mines.py
@@ -0,0 +1,34 @@
+import math
+import random
+
+from pyzzle import game
+from pyzzle import input
+from pyzzle import sprites
+from pyzzle import shapes
+from pyzzle import text
+
+
+
+WIDTH = 800
+HEIGHT = 800
+
+if __name__ == "__main__":
+	text.loadFont("trench100free.otf")
+	game.init("mines", "Mine Sweeper", WIDTH, HEIGHT)
+
+
+
+def resetBoard(board):
+	return
+
+def init():
+	print("Starting...")
+	return
+
+def update():
+	print("Updating...")
+	return
+
+def draw():
+	print("Drawing...")
+	return
