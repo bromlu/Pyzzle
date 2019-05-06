9c881328cf4b8d3f38a8d7b3be89e9fbe92fa82d
bromlu <lukewilliambrom@gmail.com>
2019-05-06 16:29:41 -0400

    Add animations

--- demos/techDemo/spaceExplorer.py
+++ demos/techDemo/spaceExplorer.py
@@ -7,11 +7,12 @@ from pyzzle import sprites
 from pyzzle import collision
 from pyzzle import shapes
 from pyzzle import text
+from pyzzle import animations
 
 player = None
-SHIP_HEIGHT = 24
-SHIP_WIDTH = 4
-SHIP_SCALE = 3
+SHIP_HEIGHT = 512
+SHIP_WIDTH = 512
+SHIP_SCALE = 0.5
 SHIP_SPEED = 10
 
 if __name__ == "__main__":
@@ -22,6 +23,30 @@ class Ship:
     def __init__(self):
         self.index = game.createGameObject()
         self.fuel = 100
+        sprites.add(self.index, "spaceship.png")
+        sprites.setFrame(self.index,0,0,SHIP_WIDTH,SHIP_HEIGHT)
+
+        self.move = animations.add(self.index)
+        self.die = animations.add(self.index)
+
+        animations.addFrame(self.index, self.move, 512, 0, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.move, 1024, 0, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.move, 1536, 0, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.move, 0, 512, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.move, 512, 512, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.move, 1024, 512, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.move, 1536, 512, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.move, 0, 1024, SHIP_WIDTH, SHIP_HEIGHT)
+
+        animations.addFrame(self.index, self.die, 512, 1024, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.die, 1024, 1024, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.die, 1536, 1024, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.die, 0, 1536, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.die, 512, 1536, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.die, 1024, 1536, SHIP_WIDTH, SHIP_HEIGHT)
+        animations.addFrame(self.index, self.die, 1536, 1536, SHIP_WIDTH, SHIP_HEIGHT)
+
+        animations.play(self.index, self.die)
 
 def init():
     global player
